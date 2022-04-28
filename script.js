e1f6ad88d43280e50b1f4a9c39cecbf249c7a231
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-04-28 13:45:45 -0300

    atualizando o requizito 07. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -1,35 +1,31 @@
 let caixaTexto = document.querySelector("#texto-tarefa");
 let botaoAdicionar = document.querySelector("#criar-tarefa");
 let listTarefa = document.querySelector("#lista-tarefas");
-let estilo = document.querySelector("body");
+
+
 
 //ADICIONANDO FUNÇÃO NO BOTAO
 botaoAdicionar.addEventListener("click", btnAdicionar);
 
 function btnAdicionar() {
-    let tarefa = caixaTexto.value;
-    let lista = document.querySelector("#lista-tarefas");
-    let item = document.createElement("li");
-    item.innerHTML = tarefa;
-    lista.appendChild(item);
-    caixaTexto.value = "";   
-     
+  let tarefa = caixaTexto.value;
+  let lista = document.querySelector("#lista-tarefas");
+  let item = document.createElement("li");
+  item.innerHTML = tarefa;
+  caixaTexto.value = "";
+//	item.addEventListener("click", mudaCor);
+  lista.appendChild(item);
+	console.log(item);
+	
 }
-//ADICIONAR COR NO FUNDO DA PAGINA
-listTarefa.addEventListener("click", mudaCor);
-
-function mudaCor() {
-    let cor = document.querySelector("body");
-    cor.style.backgroundColor = "rgb(128, 128, 128)";
-
-}
-    
-
-window.onload = () => {
-  
-}
+//ADICIONAR COR NO FUNDO 
+listTarefa.addEventListener("click", mudaCor);	
+function mudaCor(event) {
+	event.target.style.backgroundColor = "rgb(128, 128, 128)";
+		
+	}
 
 
-
+//LIMPAR ITEM SELECIONADO
 
 
