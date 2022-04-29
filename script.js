aa8e6238c35bac1597a44d4e37452af32b1a2eba
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-04-29 18:47:31 -0300

    atualizando o requizito 11 do JS e HTML. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let caixaTexto = document.querySelector("#texto-tarefa");
 let botaoAdicionar = document.querySelector("#criar-tarefa");
 let listTarefa = document.querySelector("#lista-tarefas");
 let botaoLimpar = document.querySelector("#apaga-tudo");
-
+let botaoFinalizado = document.querySelector("#remover-finalizados");
 
 
 //Adicionando item a lista
@@ -16,6 +16,7 @@ function btnAdicionar() {
   caixaTexto.value = "";
   //item.addEventListener("click", mudaCor);
   lista.appendChild(item);
+  
 
 }
 //Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)
@@ -33,6 +34,7 @@ function seleciona(event) {
   let itemSelecionado = event.target.style.backgroundColor = "rgb(128, 128, 128)";
   for (let i = 0; i < listTarefa.children.length; i += 1) {
     listTarefa.children[i].style.backgroundColor = "white";
+    
   }
   if (itemSelecionado == "rgb(128, 128, 128)") {
     event.target.style.backgroundColor = "rgb(128, 128, 128)";
@@ -49,12 +51,29 @@ function risca(event) { //essa função tanto risca como também remove e add
 		event.target.classList.add('completed');
 	} 
 }
-// Adicionar um botão que quando clicado deve apagar todos os itens da lista
+//clicado limpa os itens da lista
 botaoLimpar.addEventListener("click", btnLimpar);
 
-function btnLimpar(event){
-	listTarefa.innerHTML = "";
-		
+function btnLimpar(){
+	listTarefa.innerHTML = "";  
+  	
 }
+//clicado remove somente os elementos finalizados ou marcado da sua lista
+botaoFinalizado.addEventListener("click", btnFinalizar);
+
+function btnFinalizar(event){
+  let lista = document.querySelector("#lista-tarefas");
+  let item = document.querySelectorAll(".completed");
+  for(let i = 0; i < item.length; i++){
+    lista.removeChild(item[i]);
+    console.log(item[i]);
+  }
+  
+
+}
+//console.log(event.target.classList)
+
+
+
 
 
