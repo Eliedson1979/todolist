2fb9c00173312d74fae3e0b04566108ddb46c328
Eliedson <eliedson@MrxV3n0M.MrxV3n0M>
2022-04-29 11:06:05 -0300

    atualizando o script e style. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let listTarefa = document.querySelector("#lista-tarefas");
 
 
 
-//ADICIONANDO FUNÇÃO NO BOTAO
+//Adicionando item a lista
 botaoAdicionar.addEventListener("click", btnAdicionar);
 
 function btnAdicionar() {
@@ -13,36 +13,41 @@ function btnAdicionar() {
   let item = document.createElement("li");
   item.innerHTML = tarefa;
   caixaTexto.value = "";
-//	item.addEventListener("click", mudaCor);
+  //item.addEventListener("click", mudaCor);
   lista.appendChild(item);
-	
-	
+
 }
 //Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)
-listTarefa.addEventListener("click", mudaCor);	
+listTarefa.addEventListener("click", mudaCor);
+
 function mudaCor(event) {
-	event.target.style.backgroundColor = "rgb(128, 128, 128)";
-//	console.log(event.target);			
-	}
+  event.target.style.backgroundColor = "rgb(128, 128, 128)";
 
-//Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
-listTarefa.addEventListener("click", seleciona);	
-function seleciona(event) {
-	let itemSelecionado = event.target.style.backgroundColor = "rgb(128, 128, 128)";
-	for (let i = 0; i < listTarefa.children.length; i+=1) {
-		listTarefa.children[i].style.backgroundColor = "white";
-	}
-	if (itemSelecionado == "rgb(128, 128, 128)") {
-		event.target.style.backgroundColor = "rgb(128, 128, 128)";
-
-	}
 }
 
+//Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
+listTarefa.addEventListener("click", seleciona);
 
+function seleciona(event) {
+  let itemSelecionado = event.target.style.backgroundColor = "rgb(128, 128, 128)";
+  for (let i = 0; i < listTarefa.children.length; i += 1) {
+    listTarefa.children[i].style.backgroundColor = "white";
+  }
+  if (itemSelecionado == "rgb(128, 128, 128)") {
+    event.target.style.backgroundColor = "rgb(128, 128, 128)";
 
+  }
+}
+//Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item
+listTarefa.addEventListener("dblclick", risca);
 
+function risca(event) {
+	if(event.target.className == "completed"){
+		event.target.classList.remove('completed');
+	}else {
+		event.target.classList.add('completed');
+	}
+  console.log(event.target.className);
+}
 
-
-
-
-
+//clicando novamente duas vezes no item, deve ser possível desfazer o risco
